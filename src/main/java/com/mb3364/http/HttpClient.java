ba31815ad2bdc2aa353d2ba2b542cee118d7cf5c
Matthew Bell <matt.gts@gmail.com>
2015-05-09 02:04:16 -0700

    Response content encoding is now honored when reading the response content.

--- src/main/java/com/mb3364/http/HttpClient.java
+++ src/main/java/com/mb3364/http/HttpClient.java
@@ -3,6 +3,7 @@ package com.mb3364.http;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -26,23 +27,63 @@ public class HttpClient {
     }
 
     /**
-     * Read the input stream and convert to a string
+     * Read an InputStream and convert it to a String.
      *
      * @param inputStream InputStream to read
-     * @return String representing entire input stream contents
+     * @return String representing entire InputStream contents
+     * @throws IOException if unable to read stream
      */
-    private static String readStream(InputStream inputStream) {
+    private static String readStream(InputStream inputStream) throws IOException{
+        return readStream(inputStream, null); // No encoding, so read as binary data
+    }
+
+    /**
+     * Read an InputStream and convert it to a String.
+     *
+     * @param inputStream InputStream to read
+     * @param charsetName the charset name of the content encoding
+     * @return String representing entire InputStream contents
+     * @throws IOException if unable to read stream
+     */
+    private static String readStream(InputStream inputStream, String charsetName) throws IOException {
         if (inputStream == null) return "";
+
+        InputStreamReader reader;
+        if (charsetName != null) {
+            reader = new InputStreamReader(inputStream, charsetName);
+        } else {
+            reader = new InputStreamReader(inputStream);
+        }
+
         StringBuilder text = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                text.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            text.append(line);
         }
         return text.toString();
+    }
+
+    /**
+     * Returns the Content Encoding from the <code>content-type</code> header field.
+     *
+     * @param connection the URLConnection of the resource.
+     * @return the content encoding of the resource that the URL references, or <code>null</code> if not known.
+     */
+    private String extractContentEncoding(URLConnection connection) {
+        String contentType = connection.getContentType();
+        String charset = null;
+
+        if (contentType != null) {
+            for (String param : contentType.replace(" ", "").split(";")) {
+                if (param.startsWith("charset=")) {
+                    charset = param.split("=", 2)[1];
+                    break;
+                }
+            }
+        }
+
+        return charset;
     }
 
     protected void request(String url, HttpRequestMethod method, RequestParams params, HttpResponseHandler handler) {
@@ -102,13 +143,16 @@ public class HttpClient {
             response.setStatusMessage(responseMessage);
             response.setHeaders(responseHeaders);
 
+            // Response Content Encoding
+            String contentEncoding = extractContentEncoding(urlConnection);
+
             // 'Successful' response codes will be in interval [200,300)
             if (responseCode >= 200 && responseCode < 300) {
-                String responseContent = readStream(urlConnection.getInputStream());
+                String responseContent = readStream(urlConnection.getInputStream(), contentEncoding);
                 response.setContent(responseContent);
                 handler.onSuccess(response);
             } else {
-                String responseContent = readStream(urlConnection.getErrorStream());
+                String responseContent = readStream(urlConnection.getErrorStream(), contentEncoding);
                 response.setContent(responseContent);
                 handler.onFailure(response);
             }
