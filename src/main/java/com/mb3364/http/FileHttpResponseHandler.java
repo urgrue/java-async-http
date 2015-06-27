8c8345588b197fd05ea868243420c2b1b779d192
Matthew Bell <matt.gts@gmail.com>
2015-06-27 02:28:39 -0700

    Javadocs for response handlers.

--- src/main/java/com/mb3364/http/FileHttpResponseHandler.java
+++ src/main/java/com/mb3364/http/FileHttpResponseHandler.java
@@ -7,6 +7,46 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The response handler for the HTTP request when requesting a file download. This class is meant to
+ * be used as an anonymous inner class when making the HTTP request and will return the content as a
+ * {@link File} object.
+ * <p />
+ * Example:
+ * <pre>
+ * String url = "https://example.org/cool-file.zip";
+ * File file = new File("C:\\cool-file.zip"); // Save path
+ * HttpClient client = new AsyncHttpClient();
+ *
+ * client.get(url, params, new FileHttpResponseHandler(file) {
+ *     &#064;Override
+ *     public void onSuccess(int statusCode, Map<String, List<String>> headers, File content) {
+ *          // Request was successful
+ *     }
+ *
+ *     &#064;Override
+ *     public void onFailure(int statusCode, Map<String, List<String>> headers, File content) {
+ *          // Server responded with a status code 4xx or 5xx error
+ *     }
+ *
+ *     &#064;Override
+ *     public void onFailure(Throwable throwable) {
+ *          // An exception occurred during the request. Usually unable to connect or there was an error reading the response
+ *     }
+ *
+ *     &#064;Override
+ *     public void onProgressChanged(long bytesReceived, long totalBytes) {
+ *          // Optional: Track download progress. Will be called several times during file download
+ *          System.out.println("Downloaded: " + bytesReceived + " / " + totalBytes);
+ *     }
+ * });
+ * </pre>
+ *
+ * @see HttpResponseHandler
+ * @see StringHttpResponseHandler
+ *
+ * @author Matthew Bell
+ */
 public abstract class FileHttpResponseHandler extends HttpResponseHandler {
 
     private File file; // File containing downloaded file
@@ -33,6 +73,14 @@ public abstract class FileHttpResponseHandler extends HttpResponseHandler {
     @Override
     public abstract void onFailure(Throwable throwable);
 
+    /**
+     * Reads the body content and writes it to the specified file.
+     *
+     * @param inputStream the {@link InputStream} of the response body
+     * @param length the total length of the response body
+     * @return <code>null</code> since the content was written to the file
+     * @throws IOException if an error occurs while reading the content or writing the file
+     */
     @Override
     protected byte[] readFrom(InputStream inputStream, long length) throws IOException {
         FileOutputStream fos = new FileOutputStream(file);
